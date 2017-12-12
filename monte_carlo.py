4a61b3df85e71f6dc067750116b3ea9e2501a067
Josh Gruenstein <joshuagruenstein@gmail.com>
2017-12-12 23:15:53 +0000

    better handling of tracking properties
    include in CSV and don't require selection

--- monte_carlo.py
+++ monte_carlo.py
@@ -7,15 +7,19 @@ import os
 import json
 from datetime import datetime
 
+from collections import OrderedDict
 import matplotlib, StringIO, urllib, base64
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from matplotlib.patches import Ellipse
 
+# List of default data for simulation tracking.
 DEFAULT_STATS = ['Lateral distance', 'Lateral direction', 'Time', 'Altitude']
 
+# Master list of optional timeseries data.
 MASTER_STATS = ['Vertical velocity','Vertical acceleration','Lateral velocity','Lateral acceleration','Roll rate','Pitch rate','Yaw rate','Thrust']
 
+# Master list of Gaussian variables.
 MASTER_VARS = [
     {'name':'Rod angle','units':'degrees','defaults':[0,5]},
     {'name':'Rod direction','units':'degrees','defaults':[0,5]},
@@ -26,10 +30,12 @@ MASTER_VARS = [
     {'name':'Rod length','units':'m','defaults':[1,0.05]}
 ]
 
+# Given a list of dicts with field 'name', return the one with the given name.
 def get_prop(gauss,name):
     i = next((i for i in gauss if str(i['name'].split('(')[0].strip()) == str(name)), None)
     return random.gauss(i['mu'], i['sigma'])
 
+# Reads a logfile with a given ID to add unread points to a given list of points.
 def get_points(id,old_points):
     i = -4
     points = []
@@ -42,12 +48,35 @@ def get_points(id,old_points):
                     lastDist = sim['data']['Lateral distance'][-1]
                     lastDir = sim['data']['Lateral direction'][-1]
                     altitude = max(sim['data']['Altitude'])
-
-                    old_points.append((lastDist*math.cos(lastDir), lastDist*math.sin(lastDir), altitude))
+                    
+                    point = [('x',lastDist*math.cos(lastDir)),('y',lastDist*math.sin(lastDir)),('z',altitude)]
+                    for key in sim['data']:
+                        if not key in DEFAULT_STATS:
+                            point.extend([(key+'_min',min(sim['data'][key])),(key+'_max',max(sim['data'][key])),(key+'_mean',sum(sim['data'][key])/len(sim['data'][key]))])
+                    old_points.append(OrderedDict(point))
             i += 1
 
+# Transforms a list of dicts with identical fields into a single dict with lists of values.
+def get_aggregate(points):
+    if len(points) < 1:
+        return None
+    
+    aggregate = OrderedDict()
+    for key in points[0]:
+        aggregate[key] = []
+
+    for point in points:
+        for key in point:
+            aggregate[key].append(point[key])
+
+    return aggregate
+
+# Given a list of points, generate landing ellipses from 1 to max_std_devs standard deviations.
 def ellipses(points, max_std_devs):
-    x, y, z = zip(*points)
+    agg = get_aggregate(points)
+    x = agg['x']
+    y = agg['y']
+
     cov = np.cov(x, y)
     if not np.isnan(np.min(cov)) and not np.isinf(np.sum(cov)):
         lambda_, v = np.linalg.eig(cov)
@@ -57,6 +86,7 @@ def ellipses(points, max_std_devs):
     else:
         return []
 
+# Generate a CSV string that provides a highlights summary of a MCDA.
 def highlight_csv(points):
     csv_string = ""
 
@@ -66,12 +96,13 @@ def highlight_csv(points):
     for i, e in enumerate(ellipses(points,3)):
         csv_string += ",".join(str(k) for k in [str(i+1) + " dev",e['x'],e['y'],e['width'],e['height'],e['angle']]) + "\n"
 
-    csv_string += "\n\nx,y,z\n"
-    for point in points:
-        csv_string += ','.join([str(coord) for coord in point]) + '\n'
+    csv_string += "\n\n" + ",".join(points[0].keys()) + "\n"
+    for p in points:
+        csv_string += ",".join(str(p[key]) for key in points[0]) + "\n"
 
     return csv_string
 
+# Given a list of points, plot them and mark 3 std ellipses.  Return a base64 .png data URI.
 def plot_points(points):
     ax = plt.figure(figsize=(10,10)).add_subplot(1, 1, 1)
 
@@ -82,8 +113,8 @@ def plot_points(points):
             ell.set_edgecolor('black')
             ax.add_artist(ell)
 
-        x, y, z = zip(*points);
-        ax.scatter(x, y)
+        agg = get_aggregate(points);
+        ax.scatter(agg['x'], agg['y'])
 
     imgdata = StringIO.StringIO()
     plt.gcf().savefig(imgdata, format='png', bbox_inches='tight')
@@ -95,6 +126,7 @@ def plot_points(points):
 
     return uri
 
+# Run a dispersion analysis with given settings.  Output is a .cass file.
 def run_sims(settings):
     log_file = "logs/" + settings['id'] + ".cass"
     with open(log_file,'w') as sim_file:
